cabaa373d41016f0f65a2b092456f964a1253022
startpimp <41271417+startpimp@users.noreply.github.com>
2020-07-08 22:20:37 +0200

    0.1.3

--- src/fr/simplgame/pss/command/main/CommandDefault.java
+++ src/fr/simplgame/pss/command/main/CommandDefault.java
@@ -9,6 +9,7 @@ import fr.simplgame.pss.command.CommandMap;
 import fr.simplgame.pss.command.SimpleCommand;
 import fr.simplgame.pss.util.Loader;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.internal.entities.UserImpl;
@@ -41,17 +42,32 @@ public class CommandDefault {
 				String syntax = loader[0].lang.get("command." + command.getName() + ".syntax");
 				if (syntax.equals("NaN"))
 					syntax = "";
-				commands += "**" + command.getName() + syntax + "**" + loader[0].lang.get("general.mark.double_dot")
+				commands += "**" + command.getName() + syntax + loader[0].lang.get("general.mark.double_dot") + "**\n"
 						+ loader[0].lang.get("command." + command.getName() + ".desc") + "\n";
 			}
 		}
 
 		embed.addField(loader[0].lang.get("command.help.field1"), commands, false);
-		embed.addField(loader[0].lang.get("command.help.field2"), loader[0].lang.get("command.help.required").replace("\\n", "\n"), false);
+		embed.addField(loader[0].lang.get("command.help.field2"),
+				loader[0].lang.get("command.help.required").replace("\\n", "\n"), false);
 
 		if (!user.hasPrivateChannel())
 			user.openPrivateChannel().complete();
 		((UserImpl) user).getPrivateChannel().sendMessage(embed.build()).queue();
+	}
+
+	@Command(name = "ping", type = ExecutorType.USER)
+	public void ping(MessageChannel channel, Loader[] loader, User user) {
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setTitle(loader[0].lang.get("command.ping.title"));
+		embed.setColor(Color.decode("#ed61ce"));
+
+		embed.addField("",
+				loader[0].lang.get("command.ping.field").replace("[PING]", pss.getJda().getGatewayPing() + "")
+						.replace("[PING2]", user.getJDA().getGatewayPing() + "").replace("\\n", "\n"),
+				false);
+
+		channel.sendMessage(embed.build()).queue();
 	}
 
 }
