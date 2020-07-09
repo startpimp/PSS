48ac586b1faa9e57852c0a6a77c6cc899a55a7af
startpimp <41271417+startpimp@users.noreply.github.com>
2020-07-10 00:14:00 +0200

    0.1.3

--- src/fr/simplgame/pss/server/ServerManager.java
+++ src/fr/simplgame/pss/server/ServerManager.java
@@ -3,6 +3,7 @@ package fr.simplgame.pss.server;
 import fr.simplgame.pss.command.Command;
 import fr.simplgame.pss.command.Command.ExecutorType;
 import fr.simplgame.pss.server.jls.JoinAndLeave;
+import fr.simplgame.pss.util.CSV;
 import fr.simplgame.pss.util.Loader;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -10,10 +11,25 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 
 public class ServerManager {
 
-	@Command(name="server", type=ExecutorType.USER, description="server")
+	@Command(name = "server", type = ExecutorType.USER, description = "server")
 	public void server(String[] args, MessageChannel channel, Message message, Loader[] loader, Member member) {
-		if(args[0].equals("join")) JoinAndLeave.add(channel, message, loader, member);
+		if (args[0].equals("join"))
+			JoinAndLeave.add(channel, message, loader, member);
 	}
-	
-	
+
+	public static boolean isAuthorized(Message message, Member member, Loader loader) {
+
+		if (member.isOwner())
+			return true;
+		for (String id : CSV.getCell(message.getGuild().getId(), "moderators", "./res/server.csv").split(";")) {
+			if (id.equals(member.getId()))
+				return true;
+		}
+
+		if (!member.isOwner()) {
+			message.getChannel().sendMessage(text)
+		}
+		return false;
+	}
+
 }
