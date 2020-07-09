155d1ed01953edffff0acc96a3f04255f6e6b877
startpimp <41271417+startpimp@users.noreply.github.com>
2020-07-09 23:21:59 +0200

    Update PSS.java

--- src/fr/simplgame/pss/PSS.java
+++ src/fr/simplgame/pss/PSS.java
@@ -19,7 +19,7 @@ public class PSS implements Runnable {
 	private boolean running;
 
 	public PSS() throws LoginException {
-		PSS.jda = new JDABuilder(AccountType.BOT).setToken("NjY1NTk4NDAxMjg2NTA0NDU4.XhoBVQ.zfVBpi6pnE2PMyyzQBBdzYqRZ3Y").setActivity(Activity.listening(CommandMap.tag + "help"))
+		PSS.jda = new JDABuilder(AccountType.BOT).setToken("TOKEN").setActivity(Activity.listening(CommandMap.tag + "help"))
 				.build();
 		jda.addEventListener(new BotListener(commandMap));
 		System.out.println("[SYSTEM] PSS connected");
